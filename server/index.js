ea5b5dc92c76d693d03797ea60b7a6701d827019
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-20 13:03:24 -0300

    mas ordenamiento del codigo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import postsRoutes from "./routes/posts.routes.js"
 import {clientDB} from "./db.js"
+import {PORT} from "./config.js"
 clientDB();
 
 const app=express()
 
 app.use(postsRoutes);
 
-app.listen(3000)
-console.log("server iniciado en puerto", 3000);
+app.listen(PORT)
+console.log("server iniciado en puerto", PORT);
