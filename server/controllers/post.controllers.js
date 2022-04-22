20e3fdb87ca87cd11d48d8b94013d31dd03206d4
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-22 09:21:31 -0300

    agregado el manejo de  errores de las funciones de las solicitudes

--- server/controllers/post.controllers.js
+++ server/controllers/post.controllers.js
@@ -2,42 +2,66 @@ import Post from "../models/Post.js";
 
 
 export const getPosts = async (req, res) => {
-    const posts = await Post.find()
-    res.send(posts)
+    try {
+        const posts = await Post.find()
+        res.send(posts)
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
+    }
 };
 
 export const createPost = async (req, res) => {
-    const { title, description } = req.body
-    const newPost = new Post({ title, description })
-    console.log(newPost);
-    await newPost.save();
-
-    return res.json(newPost);
+    try {
+        const { title, description } = req.body
+        const newPost = new Post({ title, description })
+        console.log(newPost);
+        await newPost.save();
+        return res.json(newPost);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
+    }
 };
 
 export const updatePost = async (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    console.log(post)
-    return res.send("recibido");
+    try {
+        console.log(req.params);
+        console.log(req.body);
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        console.log(post)
+        return res.send(post);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
+    }
 };
 
 export const deletePost = async (req, res) => {
-    const postRemoved = await Post.findByIdAndDelete(req.params.id)
-    console.log(postRemoved)
-    if (!postRemoved) {
-        return res.sendStatus(404);
-    } else {
-        return res.sendStatus(204);
+    try {
+        const postRemoved = await Post.findByIdAndDelete(req.params.id)
+        console.log(postRemoved)
+        if (!postRemoved) {
+            return res.sendStatus(404);
+        } else {
+            return res.sendStatus(204);
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
     }
 }
 
-export const getPost =async (req, res) =>{
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-        return res.sendStatus(404);
-    } else {
-        return res.json(post);
+export const getPost = async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.sendStatus(404);
+        } else {
+            return res.json(post);
+        }
+    }catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
     }
 } 
