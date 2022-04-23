801ff53bbb45149ba86683ce031d40bfd7b8503b
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-23 08:34:50 -0300

    importado y requerido fileupload

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 import express from "express";
-import postsRoutes from "./routes/posts.routes.js"
+import postsRoutes from "./routes/posts.routes.js";
+import fileUpload from "express-fileupload";
 
 const app=express()
 
 app.use(express.json())
 app.use(postsRoutes);
+app.use(fileUpload({
+    useTempfiles:true
+}))
 
 export default app
