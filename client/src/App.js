f55f997c0c9e6ee75f63bc2c2e0cb4e052748c08
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-24 11:04:28 -0300

    instalado y configurado tailwind.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { PostForm, HomePage, NotFoundPage } from "./pages/index";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/new" element={<PostForm />}></Route>
-      <Route path="*" element={<NotFoundPage />}></Route>
-    </Routes>
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 bg-red-100 m-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/new" element={<PostForm />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
